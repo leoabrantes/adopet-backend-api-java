889cdfcb5a6d13e2960f87c3696ceb7361d7e87b
Leo Abrantes <leoabrantesmelo@gmail.com>
2023-04-04 10:30:49 -0300

    Starting Project

---
+++ src/main/java/br/com/abrantes/adopetbackendapijava/AdopetBackendApiJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.abrantes.adopetbackendapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopetBackendApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopetBackendApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/abrantes/adopetbackendapijava/AdopetBackendApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abrantes.adopetbackendapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopetBackendApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

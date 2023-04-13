cde56d94994d81ff3e97a77ae913239c56dc30ff
Leo Abrantes <leoabrantesmelo@gmail.com>
2023-04-12 22:03:19 -0300

    Shelter and Pet - atributes

--- src/main/java/br/com/abrantes/adopetbackendapijava/entities/Pet.java
+++ src/main/java/br/com/abrantes/adopetbackendapijava/entities/Pet.java
@@ -1,5 +1,124 @@
 package br.com.abrantes.adopetbackendapijava.entities;
 
-public class Pet {
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import br.com.abrantes.adopetbackendapijava.entities.enums.Size;
+import br.com.abrantes.adopetbackendapijava.entities.enums.Species;
+import br.com.abrantes.adopetbackendapijava.entities.enums.Status;
+
+@Entity
+@Table(name="tb_pet")
+public class Pet implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Double age;
+	private String personality;
+	private String picture_URL;
+	private Species species;
+	private Size size;
+	private Status status;
+	private Shelter shelter;
+	
+	public Pet() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getAge() {
+		return age;
+	}
+
+	public void setAge(Double age) {
+		this.age = age;
+	}
+
+	public String getPersonality() {
+		return personality;
+	}
+
+	public void setPersonality(String personality) {
+		this.personality = personality;
+	}
+
+	public String getPicture_URL() {
+		return picture_URL;
+	}
+
+	public void setPicture_URL(String picture_URL) {
+		this.picture_URL = picture_URL;
+	}
+
+	public Species getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(Species species) {
+		this.species = species;
+	}
+
+	public Size getSize() {
+		return size;
+	}
+
+	public void setSize(Size size) {
+		this.size = size;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Shelter getShelter() {
+		return shelter;
+	}
+
+	public void setShelter(Shelter shelter) {
+		this.shelter = shelter;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, id, personality, picture_URL, shelter, size, species, status);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pet other = (Pet) obj;
+		return Objects.equals(age, other.age) && Objects.equals(id, other.id)
+				&& Objects.equals(personality, other.personality) && Objects.equals(picture_URL, other.picture_URL)
+				&& Objects.equals(shelter, other.shelter) && size == other.size && species == other.species
+				&& status == other.status;
+	}
+	
+	
+	
+	
+	
 
 }
--- src/main/java/br/com/abrantes/adopetbackendapijava/entities/Shelter.java
+++ src/main/java/br/com/abrantes/adopetbackendapijava/entities/Shelter.java
@@ -1,5 +1,79 @@
 package br.com.abrantes.adopetbackendapijava.entities;
 
-public class Shelter {
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_shelter")
+public class Shelter implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String city;
+	private String state;
+	
+	public Shelter() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, id, name, state);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Shelter other = (Shelter) obj;
+		return Objects.equals(city, other.city) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(state, other.state);
+	}
+	
+	
 
 }
